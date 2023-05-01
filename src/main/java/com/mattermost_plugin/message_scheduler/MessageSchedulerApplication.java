3c693ba1e212373fb973964fcc4121d778d94207
Byeon sungjin <sungjin.byeon@navercorp.com>
2023-05-01 16:15:19 +0900

    initial commit

---
+++ src/main/java/com/mattermost_plugin/message_scheduler/MessageSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.mattermost_plugin.message_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mattermost_plugin/message_scheduler/MessageSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattermost_plugin.message_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
